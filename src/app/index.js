2f6b1f0b42e0dfe5246918a347cc6a8b292495b8
EternalPlayCorporation <eternalPlayCorporation@gmail.com>
2024-11-20 01:04:17 +0400

    fix:added app background img

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            <div className={"app-background"}>
+
+            </div>
+        </div>
+    );
+}
+
+
+export default App;
